e2a5c324f688332dbb38a69c4b4f2ee9f64cb1b5
Junior Taeza <taeza@usc.edu>
2026-08-29 10:29:59 -0700

    Add: duplicate-integer - submission-11

---
+++ Data Structures & Algorithms/duplicate-integer/submission-11.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        // Time: O(N), iterating through nums | Space: O(N), WC: nums is all unique
+        // set<int> s1;
+        // for(int x : nums)
+        //     s1.insert(x);
+        // return s1.size() != nums.size();
+
+        // Time: O(N), iterating through nums | Space: O(N), WC: every num is unique
+        set<int> s2;
+        for(int x : nums){
+            if(s2.contains(x))
+                return true;
+            else
+                s2.insert(x);
+        }
+        return false;
+    }
+};

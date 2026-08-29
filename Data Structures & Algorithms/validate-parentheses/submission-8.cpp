f4dad3ea26297bea9e0bee210bc4118a1f204390
Junior Taeza <taeza@usc.edu>
2026-08-29 11:14:03 -0700

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        unordered_map<char, char> parens = {
+            {')', '('},
+            {'}', '{'},
+            {']', '['}
+        };
+
+        stack<char> st;
+        for(char c : s){
+            if(c == '(' || c == '{' || c == '['){
+                st.push(c);
+            } else {
+                if(!st.empty()){
+                    char open = st.top(); st.pop();
+                    char expc = parens[c];
+                    if(expc != open)
+                        return false;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        if(!st.empty())
+            return false;
+        return true;
+    }
+};

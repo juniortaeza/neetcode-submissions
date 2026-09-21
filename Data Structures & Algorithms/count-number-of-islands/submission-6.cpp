414cc62cefe08ac20c1e57a3169030d96deedc7b
Junior Taeza <taeza@usc.edu>
2026-09-21 09:46:26 -0700

    Add: count-number-of-islands - submission-6

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-6.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private:
+    void dfs(vector<vector<char>>& grid, int r, int c) {
+        if(r < 0 || r >= grid.size())       { return; }
+        if(c < 0 || c >= grid[0].size())    { return; }
+        if(grid[r][c] != '1')               { return; }
+
+        grid[r][c] = '0';
+
+        dfs(grid, r-1, c);
+        dfs(grid, r+1, c);
+        dfs(grid, r, c-1);
+        dfs(grid, r, c+1);
+    }
+
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int numIslands = 0;
+        for(int r = 0; r < grid.size(); r++){
+            for(int c = 0; c < grid[0].size(); c++){
+                if(grid[r][c] == '1'){
+                    numIslands += 1;
+                    dfs(grid, r, c);
+                }
+            }
+        }
+        return numIslands;
+    }
+};
